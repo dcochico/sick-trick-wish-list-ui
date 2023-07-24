4a9d8aadfdc270c3186ca717123a09c4d57dcde4
Daniel Cochico <dcochico.20@gmail.com>
2023-07-24 08:26:32 -0700

    Import hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 function App() {
+  const [tricks, setTricks] = useState('');
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
