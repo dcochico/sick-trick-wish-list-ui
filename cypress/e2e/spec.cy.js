1884f4a8e40e0d092b425bb33636afb6037b05ca
Daniel Cochico <dcochico.20@gmail.com>
2023-07-24 10:06:23 -0700

    Write basic logic for main screen Cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks'), {
+      statusCode: 200,
+      fixture: 'tricks'
+    }
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should display title, form, and tricks', () => {
+    cy.get('h1').contains('Sick Trick Wish List')
+    cy.get('form').children().should('have.length', 5)
+    cy.get('.tricks-container').children().should('have.length', 3)
   })
 })
